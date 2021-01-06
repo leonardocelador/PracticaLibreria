6106590f9cfa2de1415d5236c6bb119bc49f3106
MaxSantana92 <maximiliano@lowa.com.ar>
2021-01-06 14:27:54 -0600

    boton ver listado arreglado

--- my-app/src/componentes/Listar/ListarConDatos.js
+++ my-app/src/componentes/Listar/ListarConDatos.js
@@ -16,7 +16,6 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CreateIcon from '@material-ui/icons/Create';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import './ListarConDatos.css';
 
 const ListarConDatos = ({libros, btnNuevo, Libr}) => {
 
@@ -128,6 +127,7 @@ const ListarConDatos = ({libros, btnNuevo, Libr}) => {
                           <IconButton  aria-label="delete" className={classes.margin} onClick={()=>LlamaModalEliminar(libro.Id)}>
                             <DeleteIcon fontSize="small" />
                           </IconButton>
+
                         </Grid>
                       </Grid>
                         
